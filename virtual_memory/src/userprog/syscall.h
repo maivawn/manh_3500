4b8df1dd89a8b54bfd79b75ac675c1a6bdb83d1c
manh3500 <98152725+maivawn@users.noreply.github.com>
2023-12-18 13:32:29 +0700

    Delete virtual_memory directory

--- virtual_memory/src/userprog/syscall.h
+++
@@ -1,8 +0,0 @@
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-
-#define USER_VADDR_BOTTOM ((void *) 0x08048000)
-
-void syscall_init (void);
-void syscall_exit (void);
-#endif /* userprog/syscall.h */
